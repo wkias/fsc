de573c4c90464d571d59553c5ce0cc6653450629
wikas <willicen@qq.com>
2020-06-13 14:08:43 +0800

    舵机控制初步可用

--- App/adc_sampling.c
+++ App/adc_sampling.c
@@ -43,20 +43,58 @@ void adc_sampling()
     ad_sum -= max;
     ad_sum -= min;
     adc_val[i] = ad_sum / ADC_SAMPLING_FREQ;
-    adc_val[i] = (adc_val[i] - min) / (max - min) * 100; //归一化
     ad_sum = 0;
   }
 
-  //计算电感和中线的距离-cm
+#ifdef INDUCTOR_CENTER_DISTANCE
+  //计算电感和中线的距离-mm
   for (int8 i = 0; i < 6; i++)
   {
     //电感值和中线偏差非线性关系，差比和作偏差不稳定
-    arm_sqrt_f32((adc_slope * adc_height / adc_val[i]), adc_val + i);
+    adc_val[i] = solve(adc_val[i]) * 100;
   }
 
-  //中线偏差-cm
-  adc_bias[0] = (adc_val[5] - adc_val[0]) / 2;//垂直电感
-  adc_bias[1] = (adc_val[4] - adc_val[1]) / 2;//水平电感-边缘
-  adc_bias[2] = (adc_val[3] - adc_val[2]) / 2;//水平电感-中间
-
+  //中线偏差-mm
+  adc_bias[0] = (adc_val[5] - adc_val[0]) / 2; //垂直电感
+  adc_bias[1] = (adc_val[4] - adc_val[1]) / 2; //水平电感-边缘
+  adc_bias[2] = (adc_val[3] - adc_val[2]) / 2; //水平电感-中间
+#else
+  //中线偏差-无量纲量
+  adc_bias[0] = (adc_val[5] - adc_val[0]) / (adc_val[5] + adc_val[0]) * 100; //垂直电感
+  adc_bias[1] = (adc_val[4] - adc_val[1]) / (adc_val[4] + adc_val[1]) * 100; //水平电感-边缘
+  adc_bias[2] = (adc_val[3] - adc_val[2]) / (adc_val[3] + adc_val[2]) * 100; //水平电感-中间
+#endif
 }
+
+#ifdef INDUCTOR_CENTER_DISTANCE
+//解三次函数实根
+float32_t solve(float32_t adc_val)
+{
+  return adc_height * adc_height / sqrt_3(-27 * adc_val / (2 * adc_slope) + sqrt_(729 * adc_val * adc_val / adc_slope / adc_slope + 108 * adc_height * adc_height * adc_height * adc_height * adc_height * adc_height) / 2) -
+         sqrt_3(-27 * adc_val / (2 * adc_slope) + sqrt_(729 * adc_val * adc_val / adc_slope / adc_slope + 108 * adc_height * adc_height * adc_height * adc_height * adc_height * adc_height) / 2) / 3;
+}
+
+//解平方根
+float32_t sqrt_(float32_t x)
+{
+  float32_t y;
+  arm_sqrt_f32(x, &y);
+  return y;
+}
+
+//牛顿迭代求三次方根近似值，解范围[-10000,10000]
+float32_t sqrt_3(float32_t x)
+{
+  float64_t l = -10000, r = 10000, mid;
+  //浮点数二分
+  while (r - l > 1e-7)
+  {
+    mid = (l + r) / 2;
+    if ((mid * mid * mid) >= x)
+      r = mid;
+    else
+      l = mid;
+  }
+  return l;
+}
+#endif
--- App/servo.c
+++ App/servo.c
@@ -8,16 +8,20 @@ float32_t servo_bias_wight[3] = {SERVO_ERRORS_WEIGHT_0,
                                  SERVO_ERRORS_WEIGHT_2};
 float32_t servo_bias[3] = {0}; //本次中线误差，上次中线误差，累计误差和
 float32_t servo_correct = 0;   //舵机误差增量修正值
-float32_t servo_angle = 0;     //舵机偏转角度
 float32_t servo_out = 0;       //舵机PWM占空比
 float32_t ratio = PARAMENTER_SERVO_MOTOR_RATIO;
-float32_t adc_bias_abs[3];
 
 void servo()
 {
+#ifdef INDUCTOR_CENTER_DISTANCE
     //根据中线距离计算车轮转动角度，然后计算对应的占空比
     servo_bias[0] = (adc_bias_abs[0] - adc_bias_abs[1] > 5) ? adc_bias[0] : (adc_bias[1] + adc_bias[2]) / 2;
     servo_out = arctan(servo_bias[0] / ADC_SAMPLING_PARAMETER_FORWARD) / SERVO_ANGLE_LIMIT * SERVO_DUTY_INTERVAL_LIMIT + SERVO_BASE_POINT;
+#else
+    servo_bias[0] = servo_bias_wight[0] * adc_bias[0] +
+                    servo_bias_wight[1] * adc_bias[1] +
+                    servo_bias_wight[2] * adc_bias[2];
+#endif
 
     //位置PID，中线误差修正
     servo_correct = servo_pid_param[0] * servo_bias[0] +
@@ -25,7 +29,7 @@ void servo()
                     servo_pid_param[2] * (servo_bias[0] - servo_bias[1]);
     servo_bias[1] = servo_bias[0];
     servo_bias[2] += servo_bias[0];
-    // servo_out += servo_correct;
+    servo_out = SERVO_BASE_POINT + servo_correct;
 
     //限幅输出
     servo_out = (servo_out > SERVO_LEFT_LIMIT) ? servo_out : SERVO_LEFT_LIMIT;
@@ -33,13 +37,14 @@ void servo()
     ftm_pwm_duty(PORT_SERVO, FTM_CH0, servo_out);
 
     //根据舵机偏转幅度计算电机期望速度
-    expected_motor_out = SERVO_BASE_POINT - servo_out;//期望输出暂时作中间变量
+    expected_motor_out = SERVO_BASE_POINT - servo_out; //期望输出暂时作中间变量
     expected_motor_out = (expected_motor_out > 0) ? expected_motor_out : -expected_motor_out;
     expected_motor_out = MOTOR_VELOCITY_SUPERIOR_LIMIT - expected_motor_out * ratio;
     expected_motor_out = (expected_motor_out > MOTOR_VELOCITY_SUPERIOR_LIMIT) ? MOTOR_VELOCITY_SUPERIOR_LIMIT : expected_motor_out;
     expected_motor_out = (expected_motor_out < MOTOR_VELOCITY_INFERIOR_LIMIT) ? MOTOR_VELOCITY_INFERIOR_LIMIT : expected_motor_out;
 }
 
+#ifdef INDUCTOR_CENTER_DISTANCE
 float64_t arctan(float64_t x)
 {
     float64_t mult, sum, xx;
@@ -70,3 +75,4 @@ float64_t arctan(float64_t x)
         return sum;
     }
 }
+#endif
