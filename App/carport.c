6849b73e4ca8f2ae10e58f5a4942df91c782bd60
duyaning <1083005883@qq.com>
2020-07-27 14:21:26 +0800

    蜂鸣器一直响找原因

--- App/carport.c
+++ App/carport.c
@@ -11,14 +11,14 @@ void carport(void)
 // 进入车库
 void go_home()
 {
-    gpio_set(PORT_BEEPER, 1);
+    // gpio_set(PORT_BEEPER, 1);
     disable_irq(PIT1_IRQn);
     disable_irq(PIT2_IRQn);
     ftm_pwm_duty(PORT_MOTOR, FTM_CH2, 0);
     DELAY_MS(1000);
     ftm_pwm_duty(PORT_MOTOR, FTM_CH3, MOTOR_VELOCITY_BASE_POINT);
     ftm_pwm_duty(PORT_SERVO, FTM_CH0, SERVO_LEFT_LIMIT);
-    gpio_set(PORT_BEEPER, 0);
+    // gpio_set(PORT_BEEPER, 0);
     DELAY_MS(3000);
     ftm_pwm_duty(PORT_MOTOR, FTM_CH3, 0);
 }
